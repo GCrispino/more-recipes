51c69ebb00aa2a8d49ca83054a4563fd4dfc7a66
Crispino <gabriel.crispino@hotmail.com>
2019-02-03 12:44:20 -0300

    Refactor signin.spec.js with testValidator functions

--- server/tests/unit/validators/signin.spec.js
+++ server/tests/unit/validators/signin.spec.js
@@ -1,31 +1,28 @@
 //@ts-check
+import { testValidator,testValidatorAsync } from './utils';
 import validators from '../../../validators'
 const { SignInUserValidator } = validators;
 
+const testSignInUserValidator = (validatorObj,validatorFunctionName,beforeAction) => 
+  testValidator(SignInUserValidator,validatorObj,validatorFunctionName,beforeAction);
+
+const testSignInUserValidatorAsync = (validatorObj,validatorFunctionName,beforeAction) => 
+  testValidatorAsync(SignInUserValidator,validatorObj,validatorFunctionName,beforeAction);
 
 describe('The SignInUserValidator class', () => {
 
   describe('The validateEmail function',() => {
 
     test('Should return invalid email error if email is invalid', async () => {
-
-      const validator = new SignInUserValidator({
+      const [ _,__,errors ] = await testSignInUserValidatorAsync({
         email: 'bademail@gmail'
-      });
-
-      await validator.validateEmail();
-
-      const { errors } = validator;
+      },'validateEmail');
 
       expect(errors).toEqual(['The email must be a valid email address.']);
     });
 
     test('Should return required error if email is empty', async () => {
-      const validator = new SignInUserValidator({});
-
-      await validator.validateEmail();
-
-      const { errors } = validator;
+      const [ _,__,errors ] = await testSignInUserValidatorAsync({},'validateEmail');
 
       expect(errors).toEqual(['The email is required.']);
     });
@@ -34,11 +31,7 @@ describe('The SignInUserValidator class', () => {
 
   describe('The validatePassword function',() => {
     test('Should return required error if password is empty',() => {
-      const validator = new SignInUserValidator({});
-  
-      validator.validatePassword();
-  
-      const { errors } = validator;
+      const [ _,__,errors ] = testSignInUserValidator({},'validatePassword');
   
       expect(errors).toEqual(['The password is required.']);
     });
@@ -47,55 +40,41 @@ describe('The SignInUserValidator class', () => {
   describe('The isValid function',() => {
 
     test('Should fail when empty user is passed', async () => {
-      const validator = new SignInUserValidator({});
-  
-      const result = await validator.isValid();
+      const [ _,result ] = testSignInUserValidator({},'isValid');
   
       expect(result).toBe(false);
     });
     
     test('Should fail when user with invalid email is passed', async () => {
-      const validator = new SignInUserValidator({
+      const [ _,result ] = await testSignInUserValidatorAsync({
         email: 'test@test',
         password: '1234567'
-      });
-  
-      const result = await validator.isValid();
-  
+      },'isValid');
   
       expect(result).toBe(false);
     });
 
     test('Should fail when user with no email is passed', async () => {
-      const validator = new SignInUserValidator({
+      const [ _,result ] = await testSignInUserValidatorAsync({
         password: '1234567'
-      });
-  
-      const result = await validator.isValid();
-  
+      },'isValid');
   
       expect(result).toBe(false);
     });
 
     test('Should fail when user with no password is passed', async () => {
-      const validator = new SignInUserValidator({
+      const [ _,result ] = await testSignInUserValidatorAsync({
         email: 'test@test.com'
-      });
-  
-      const result = await validator.isValid();
-  
+      },'isValid');
   
       expect(result).toBe(false);
     });
 
     test('Should be successful when user passes valid email and password', async () => {
-      const validator = new SignInUserValidator({
+      const [ _,result ] = await testSignInUserValidatorAsync({
         email: 'test@test.com',
         password: '1234567'
-      });
-  
-      const result = await validator.isValid();
-  
+      },'isValid');
   
       expect(result).toBe(true);
     });
