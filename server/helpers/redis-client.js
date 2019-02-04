2d0c1128b8d827e2f7ce8db76106b8d148d795b7
Crispino <gabriel.crispino@hotmail.com>
2019-02-04 09:58:35 -0300

    Add redis-mock support

--- server/helpers/redis-client.js
+++ server/helpers/redis-client.js
@@ -1,16 +1,19 @@
-import { createClient } from 'then-redis';
-//  import FakeRedis from './redis/fakeRedis';
+import { createClient } from "then-redis";
+import { createClient as createClientMock } from "redis-mock";
 /**
  * Create a new client redis
  * @returns {object} fake-redis client for testing and real redis-client in production
  */
 const getRedisClient = () => {
-  if (process.env.NODE_ENV === 'production') {
+  if (process.env.NODE_ENV === "production") {
     return createClient(process.env.REDIS_URL);
+  }
+
+  if (process.env.NODE_ENV === "test") {
+    return createClientMock();
   }
 
   return createClient();
 };
-
 
 export default getRedisClient();
