477728c6da36d664b6ca9db44145a3b1d744a2c1
Crispino <gabriel.crispino@hotmail.com>
2019-02-06 14:28:32 -0300

    Finish signin integration test suite

--- server/tests/integration/users/login.spec.js
+++ server/tests/integration/users/login.spec.js
@@ -24,4 +24,68 @@ describe("The user login", () => {
     expect(response.body.data.user.email).toBe(fakeUser.email);
     expect(response.body.data.access_token).toBeTruthy();
   });
+
+  test("Should return validation error if email is not provided", async () => {
+    const fakeUser = {
+      name: "testuser",
+      password: "1234567"
+    };
+
+    const response = await supertest(app)
+      .post("/api/v1/users/signin")
+      .send(fakeUser)
+      .expect(422);
+
+    expect(response.body.status).toBe("fail");
+    expect(response.body.data).toEqual({ errors: ["The email is required."] });
+  });
+
+  test("Should return validation error if email is not valid", async () => {
+    const fakeUser = {
+      name: "testuser",
+      email: "bad@email",
+      password: "1234567"
+    };
+
+    const response = await supertest(app)
+      .post("/api/v1/users/signin")
+      .send(fakeUser)
+      .expect(422);
+
+    expect(response.body.status).toBe("fail");
+    expect(response.body.data).toEqual({
+      errors: ["The email must be a valid email address."]
+    });
+  });
+
+  test("Should return validation error if password is not provided", async () => {
+    const fakeUser = {
+      name: "testuser",
+      email: faker.internet.email()
+    };
+
+    const response = await supertest(app)
+      .post("/api/v1/users/signin")
+      .send(fakeUser)
+      .expect(422);
+
+    expect(response.body.status).toBe("fail");
+    expect(response.body.data).toEqual({
+      errors: ["The password is required."]
+    });
+  });
+
+  test("Should return validation errors if password and email are not provided", async () => {
+    const fakeUser = {};
+
+    const response = await supertest(app)
+      .post("/api/v1/users/signin")
+      .send(fakeUser)
+      .expect(422);
+
+    expect(response.body.status).toBe("fail");
+    expect(response.body.data).toEqual({
+      errors: ["The password is required.", "The email is required."]
+    });
+  });
 });
