be8672901d78ab01aedde0c01fe3031703d62dba
Crispino <gabriel.crispino@hotmail.com>
2019-02-06 10:52:58 -0300

    Finish integration test for registering user

--- server/tests/integration/users/signup.spec.js
+++ server/tests/integration/users/signup.spec.js
@@ -1,17 +1,26 @@
 import supertest from "supertest";
 import faker from "faker";
 import app from "../../../index";
+import { User } from "../../../database/models";
 
 describe("The user signup test", () => {
   test("should register a new user", async () => {
+    const fakeUser = {
+      name: "Test user",
+      email: faker.internet.email(),
+      password: "1234567"
+    };
     const response = await supertest(app)
       .post("/api/v1/users/signup")
-      .send({
-        name: "Test user",
-        email: faker.internet.email(),
-        password: "1234567"
-      });
+      .send(fakeUser)
+      .expect(200);
 
-    console.log(response.body);
+    expect(response.body.data.user.email).toBe(fakeUser.email);
+    expect(response.body.data.user.name).toBe(fakeUser.name);
+    expect(response.body.data.access_token).toBeDefined();
+
+    const userfromDB = User.find({ where: { email: fakeUser.email } });
+
+    expect(userfromDB).toBeTruthy();
   });
 });
