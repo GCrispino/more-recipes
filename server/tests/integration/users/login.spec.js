1c661c89e56d42cf885e2f4554753a59ad525b8f
Crispino <gabriel.crispino@hotmail.com>
2019-02-07 11:53:14 -0300

    Refactor some tests

--- server/tests/integration/users/login.spec.js
+++ server/tests/integration/users/login.spec.js
@@ -1,27 +1,25 @@
 import supertest from "supertest";
 import faker from "faker";
-import bcrypt from "bcrypt";
 import app from "../../../index";
-import { User } from "../../../database/models";
+import { generateUser } from "../../utils/generate";
 
 describe("The user login", () => {
   test("The user can login and get a jwt", async () => {
-    const password = "1234567";
-    const fakeUser = {
-      name: "testuser",
-      email: faker.internet.email(),
-      password: await bcrypt.hash(password, 1)
-    };
-
-    await User.create(fakeUser);
+    const {
+      fakeUser: { password },
+      user: { email }
+    } = await generateUser();
 
     const response = await supertest(app)
       .post("/api/v1/users/signin")
-      .send({ ...fakeUser, password })
+      .send({
+        email,
+        password
+      })
       .expect(200);
 
     expect(response.body.status).toBe("success");
-    expect(response.body.data.user.email).toBe(fakeUser.email);
+    expect(response.body.data.user.email).toBe(email);
     expect(response.body.data.access_token).toBeTruthy();
   });
 
--- server/tests/unit/middleware/auth.spec.js
+++ server/tests/unit/middleware/auth.spec.js
@@ -3,19 +3,14 @@ import jwt from "jsonwebtoken";
 import middleware from "../../../middleware";
 import config from "../../../config";
 import { User } from "../../../database/models";
+import { generateUser } from "../../utils/generate";
 
 const { auth } = middleware;
 
 describe("The auth middleware", () => {
   test("Should call next if user is authenticated", async () => {
-    const user = {
-      name: "test",
-      password: "12345678",
-      email: faker.internet.email()
-    };
-    await User.create(user);
+    const { user, token } = await generateUser();
 
-    const token = jwt.sign(user, config.JWT_SECRET);
     const req = {
       body: { access_token: token }
     };
@@ -37,12 +32,7 @@ describe("The auth middleware", () => {
   });
 
   test("Should call sendFailureResponse if user is not authenticated", async () => {
-    const user = {
-      name: "test",
-      password: "12345678",
-      email: faker.internet.email()
-    };
-    await User.create({ ...user, email: faker.internet.email() });
+    await generateUser();
 
     const req = {
       body: {},
